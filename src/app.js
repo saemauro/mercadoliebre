b826dd381963711516b9b5dbbffe0e445743642d
saemauro <maurosaegh@gmail.com>
2023-02-17 23:23:14 -0300

    act 3

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "views/index.html"))
 })
 
+app.get("/login", function(req, res){
+    res.sendFile(path.join(__dirname, "views/login.html"))
+})
+
+app.get("/register", function(req, res){
+    res.sendFile(path.join(__dirname, "views/register.html"))
+})
+
 app.listen(3000, function (){
     console.log("Servidor escuchando en puerto 3000");
 });
